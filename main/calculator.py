d99ce2692146d3b49d6eb589a1a7512521795101
Algeot <takuto_ogawa_1729@outlook.jp>
2022-12-20 00:10:46 +0900

    Add calculator

---
+++ main/calculator.py
@@ -0,0 +1,10 @@
+﻿class circle:
+    def __init__(self, x, y, r):
+        self.x, self.y, self.r = x, y, r
+        return
+    
+    def __add__(self, other):
+        x = (self.x + other.x) // 2
+        y = (self.y + other.y) // 2
+        r = max(self.r, other.r)
+        return circle(x, y, r)
