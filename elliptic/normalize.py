f3ed3a268d1c94c36ba8f54b10f32e6c2bffb816
Algeot <takuto_ogawa_1729@outlook.jp>
2022-12-20 00:30:04 +0900

    add elliptic curve

---
+++ elliptic/normalize.py
@@ -0,0 +1,8 @@
+﻿class elliptic:
+    def __init__(self, a1, a3, a2, a4, a6):
+        self.A = [0] * 7
+        self.A[1] = a1
+        self.A[3] = a3
+        self.A[2] = a2
+        self.A[4] = a4
+        self.A[6] = a6
